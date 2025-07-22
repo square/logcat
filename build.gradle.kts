0d0444e72f54e22f2f713884ea7aa62275305e32
Jon Poulton <jpoulton@pm.me>
2025-07-22 20:44:55 +0100

    Bump to kotlin 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.binary.compatibility.validator) apply false
+  alias(libs.plugins.dokka) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.kotlin.multiplatform) apply false
+  alias(libs.plugins.maven.publish) apply false
   alias(libs.plugins.ktlint)
 }
 
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
@@ -11,8 +13,10 @@ android {
     targetCompatibility = JavaVersion.VERSION_1_8
   }
 
-  kotlinOptions {
-    jvmTarget = "1.8"
+  kotlin {
+    compilerOptions {
+      jvmTarget = JvmTarget.JVM_1_8
+    }
   }
 
   defaultConfig {
