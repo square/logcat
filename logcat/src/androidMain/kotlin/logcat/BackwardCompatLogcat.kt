1588275973c65e185077f6e04fdb34ea68f5e8b7
Dewan Tawsif <theonetawsif@gmail.com>
2025-07-23 22:44:56 +0600

    Deprecate 'outerClassSimpleNameInternalOnlyDoNotUseKThxBye'

--- logcat/src/androidMain/kotlin/logcat/BackwardCompatLogcat.kt
+++ logcat/src/androidMain/kotlin/logcat/BackwardCompatLogcat.kt
@@ -7,6 +7,7 @@ import kotlin.jvm.JvmName
 import logcat.internal.outerClassSimpleName
 
 // Kept here for backward compat purposes
+@Deprecated("This is an internal logcat API that should not be used from outside of logcat.")
 @OptIn(InternalLogcatApi::class)
 @Suppress("unused")
 @PublishedApi
