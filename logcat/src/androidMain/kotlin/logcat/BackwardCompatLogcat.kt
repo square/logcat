7ab3ca82d15bca049b999c7df952747a1a43b27f
Dewan Tawsif <theonetawsif@gmail.com>
2025-07-23 06:12:01 +0600

    Add missing OptIn for outerClassSimpleNameInternalOnlyDoNotUseKThxBye

--- logcat/src/androidMain/kotlin/logcat/BackwardCompatLogcat.kt
+++ logcat/src/androidMain/kotlin/logcat/BackwardCompatLogcat.kt
@@ -7,6 +7,7 @@ import kotlin.jvm.JvmName
 import logcat.internal.outerClassSimpleName
 
 // Kept here for backward compat purposes
+@OptIn(InternalLogcatApi::class)
 @Suppress("unused")
 @PublishedApi
 internal fun Any.outerClassSimpleNameInternalOnlyDoNotUseKThxBye(): String = outerClassSimpleName()
